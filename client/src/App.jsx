7393f255b3ed225c3f2160e5d6f75adf19e5777c
MF <mfregwork21@gmail.com>
2025-02-11 21:12:21 -0800

    First Challenge

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+// import Greet from "./components/Greet"
+
+import Header from "./components/Header"
+import MainContent from "./components/MainContent"
+import Footer from "./components/Footer"
+
+function App(){
+  return (
+    <>
+      {/* <Greet/> */}
+      <Header/>
+      <MainContent/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
